66f10e77a577e4eff5e1405aa696f388afe99027
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-07-03 01:04:36 +0300

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import redis from 'redis';
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    const value = await asyncGet(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
