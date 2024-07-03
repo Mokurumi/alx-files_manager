26a32c23aa97020dbde64ddbdfadcf39072b695b
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-07-04 00:58:08 +0300

    first api

---
+++ controllers/AppController.js
@@ -0,0 +1,36 @@
+/**
+ * Inside server.js, create the Express server:
+
+it should listen on the port set by the environment variable PORT or by default 5000
+it should load all routes from the file routes/index.js
+Inside the folder routes, create a file index.js that contains all endpoints of our API:
+
+GET /status => AppController.getStatus
+GET /stats => AppController.getStats
+Inside the folder controllers, create a file AppController.js that contains the definition of the 2 endpoints:
+
+GET /status should return if Redis is alive and if the DB is alive too by using the 2 utils created previously: { "redis": true, "db": true } with a status code 200
+GET /stats should return the number of users and files in DB: { "users": 12, "files": 1231 } with a status code 200
+users collection must be used for counting all users
+files collection must be used for counting all files
+
+I am currently in the AppController.js file. I will create the AppController class and add the getStatus and getStats methods. */
+
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+    res.status(200).send({ redis: redisStatus, db: dbStatus });
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).send({ users, files });
+  }
+}
+
+export default AppController;
